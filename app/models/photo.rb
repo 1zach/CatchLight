3f1712459d0404b9d29fd0fb3d4ac3e716078423
stephane lorio <lorio.stephane@gmail.com>
2022-08-30 15:19:02 +0200

    finished the basic map without extra features

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  belongs_to :user, optional: true
+  belongs_to :user
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_photos, through: :bookmarks
   # Geocoding
