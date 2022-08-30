093bd0302cf68e944d1b97a1cd2e8d766680570a
tenzin zayn gombo <tenzinzayn007@gmail.com>
2022-08-30 16:40:11 +0200

    creation of route and controller for the about us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
-    
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     resources :users, only: :show
     resources :photos do
-    resources :bookmarks
+      resources :bookmarks
     end
-  
-   
+
+
+    resources :about_us, only: :index
+
+
     #resources :bookmarks, only: :destroy
 
   # Defines the root path route ("/")
