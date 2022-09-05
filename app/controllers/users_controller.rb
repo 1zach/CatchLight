8f89012e827642ff6442382026d2ddb48e31c5fb
(Ambroise Buckens) buckens93@gmail.com <buckens93@gmail.com>
2022-09-05 14:19:52 +0200

    changing the hall user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,5 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @trips_with_photos = current_user.photos.group_by(&:trip)
   end
 end
--- db/migrate/20220902105511_add_trip_column_to_photos.rb
+++
@@ -1,5 +0,0 @@
-class AddTripColumnToPhotos < ActiveRecord::Migration[7.0]
-  def change
-    add_column :photos, :trip, :string
-  end
-end
